097af3f69a525f47ad7f9abbfea53bb05bec6e60
Timur Shapovalov <sapovalovtimurs@gmail.com>
2025-08-19 01:23:44 +0300

    Finaly scraping

--- excelmodule.py
+++ excelmodule.py
@@ -1,10 +1,17 @@
 import xlsxwriter
 
-workbook = xlsxwriter.Workbook("shoes.xlsx")
-worksheet = workbook.add_worksheet()
+def add_to_excel(scrap):
+    workbook = xlsxwriter.Workbook("shoes.xlsx")
+    worksheet = workbook.add_worksheet()
 
-row = 0
-col = 0
+    row = 0
+    col = 0
 
+    worksheet.set_column("A:A",100)
+    worksheet.set_column("B:B",15)
+    for name,price in scrap():
+        worksheet.write(row,col,name)
+        worksheet.write(row,col+1,price)
+        row +=1
 
-workbook.close()
+    workbook.close()
--- scraping.py
+++ scraping.py
@@ -1,7 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+import excelmodule
+from time import sleep
 
-url = "https://sneakerstore.by/muzhskie-krossovki/"
+def check_product():
+    url = "https://sneakerstore.by/muzhskie-krossovki/"
+    headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:141.0) Gecko/20100101 Firefox/141.0"}
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text,features="html.parser")
+    response = requests.get(url,headers = headers)
+    soup = BeautifulSoup(response.text, features="html.parser")
+
+    page = 1
+    MAX_PRICE = 150
+    products = soup.find_all("li", class_="catalogue__products-list-item")
+
+    while len(products) != 0:
+        for product in products:
+            price = int(product.find_all("span",class_= "catalogue__price catalogue__price--sm")[-1].text.split(".")[0])
+            if price<=MAX_PRICE:
+                name = product.find("a",class_="catalogue__product-name products-list__name").find("span").text
+                yield name,price
+        page += 1
+        response = requests.get(url + f"?page={page}",headers=headers)
+        soup = BeautifulSoup(response.text, features="html.parser")
+        products = soup.find_all("li", class_="catalogue__products-list-item")
+
+excelmodule.add_to_excel(check_product)
